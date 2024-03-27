1247dca0d1b100f4172a8a6e09056ad91de47922
Estagiario 3 <estagiario3@movtech.com.br>
2024-03-27 17:32:51 -0300

    ta bom demais isso aqui

--- script.js
+++ script.js
@@ -1,40 +1,91 @@
 const hourDigital = document.querySelector('#hour-digital');
 const minDigital = document.querySelector('#min-digital');
+const periodDigital = document.querySelector('#period')
 const secDigital = document.querySelector('#sec-digital');
 
-const hourAnalog = document.querySelector('.hour-analog');
-const minAnalog = document.querySelector('.min-analog');
-const secAnalog = document.querySelector('.sec-analog');
+const formatSwitchBtn = document.querySelector(".format-switch-btn");
 
-function getTime(){
-    const time = new Date();
+formatSwitchBtn.addEventListener("click", () => {
+    formatSwitchBtn.classList.toggle("active");
 
-    const getHoursRot = (360 / 12) * time.getHours();
-    const getMinRot = (360 / 60) * time.getMinutes();
-    const getSecRot = (360 / 60) * time.getSeconds();
+    var formatValue = formatSwitchBtn.getAttribute("data-format");
 
-    hourAnalog.style.transform = `rotate(${getHoursRot}deg)`;
-    minAnalog.style.transform = `rotate(${getMinRot}deg)`;
-    secAnalog.style.transform = `rotate(${getSecRot}deg)`;
-}
-setInterval(() =>{
-    getTime()
-}, 1000);
+    if (formatValue === "12") {
+        formatSwitchBtn.setAttribute("data-format", "24");
+    }
+    else {
+        formatSwitchBtn.setAttribute("data-format", "12");
+    }
+});
 
-getTime();
-
-setInterval(() =>{
+setInterval(() => {
     let date = new Date();
     let dHour = date.getHours();
     let dMinute = date.getMinutes();
     let dSec = date.getSeconds();
+    let period = "AM";
 
-    hourDigital .innerHTML = `${formatTime(dHour)}`;
+    if (dHour >= 12) {
+        period = "PM"
+    }
+
+    var formatValue = formatSwitchBtn.getAttribute("data-format");
+    if (formatValue === "12") {
+        dHour = dHour > 12 ? dHour % 12 : dHour;
+    }
+
+    hourDigital.innerHTML = `${formatTime(dHour)}`;
     minDigital.innerHTML = `${formatTime(dMinute)}`;
+    periodDigital.innerHTML = `${formatTime(period)}`;
     secDigital.innerHTML = `${formatTime(dSec)}`;
-},1000);
 
-function formatTime(time){
+}, 1000);
+
+function formatTime(time) {
     return time < 10 ? '0' + time : time;
 }
+
+const dotmenuBtn = document.querySelector(".dot-menu-btn");
+const dotMenu = document.querySelector(".dot-menu");
+
+dotmenuBtn.addEventListener("click", () => {
+    dotMenu.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+    if (e.target.id !== "active-menu") {
+        dotMenu.classList.remove("active");
+    }
+})
+
+const digitalClockBtn = document.getElementById('botaodigital');
+const analogClockBtn = document.getElementById('botaoanalog');
+const digitalClock = document.querySelector('#digital-clock');
+const analogClock = document.querySelector('#analog-clock');
+
+digitalClockBtn.addEventListener('click', () => {
+    digitalClock.style.display = 'flex';
+    analogClock.style.display = 'none';
+});
+
+analogClockBtn.addEventListener('click', () => {
+    digitalClock.style.display = 'none';
+    analogClock.style.display = 'flex';
+});
+
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sec = document.querySelector('#sec');
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
+}, 1000); 
 
