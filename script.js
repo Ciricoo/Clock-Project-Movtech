d991ac292a3d1ac66a8eba458f778e664f42974c
Estagiario 3 <estagiario3@movtech.com.br>
2024-03-28 17:32:15 -0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+//Digital Clock
+
 const hourDigital = document.querySelector('#hour-digital');
 const minDigital = document.querySelector('#min-digital');
 const periodDigital = document.querySelector('#period')
@@ -58,20 +61,7 @@ document.addEventListener("click", (e) => {
     }
 })
 
-const digitalClockBtn = document.getElementById('botaodigital');
-const analogClockBtn = document.getElementById('botaoanalog');
-const digitalClock = document.querySelector('#digital-clock');
-const analogClock = document.querySelector('#analog-clock');
-
-digitalClockBtn.addEventListener('click', () => {
-    digitalClock.style.display = 'flex';
-    analogClock.style.display = 'none';
-});
-
-analogClockBtn.addEventListener('click', () => {
-    digitalClock.style.display = 'none';
-    analogClock.style.display = 'flex';
-});
+//Analog Clock
 
 const deg = 6;
 const hr = document.querySelector('#hr');
@@ -89,3 +79,116 @@ setInterval(() => {
     sec.style.transform = `rotateZ(${ss}deg)`;
 }, 1000); 
 
+// Chronometer
+
+const timerEl = document.getElementById('timer');
+const marksList = document.getElementById('marks-list');
+let intervalId = 0;
+let timer = 0;
+let marks = [];
+
+function formatarTime  (time) {
+    const hours = Math.floor(time / 360000);
+    const minutes = Math.floor((time % 360000) / 6000);
+    const seconds = Math.floor((time % 6000) / 100);
+    const hundredths = time % 100;
+
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${hundredths.toString().padStart(2, '0')}`;
+};
+
+ function setTimer (time) {
+    timerEl.innerText = formatarTime(time);
+};
+
+function addMarkToList (markIndex, markTime) {
+    const markItem = document.createElement('div');
+    markItem.classList.add('trash');
+    markItem.innerHTML = `<p>Marca ${markIndex}: ${formatarTime(markTime)}</p>`;
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('botao-trash');
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    deleteButton.addEventListener('click', () => {
+        marks.splice(markIndex - 1, 1); 
+        updateMarksList(); 
+    });
+
+    markItem.appendChild(deleteButton);
+    marksList.appendChild(markItem);
+};
+
+
+function updateMarksList () {
+    marksList.innerHTML = ''; 
+    marks.forEach((mark, index) => {
+        addMarkToList(index + 1, mark);
+    });
+};
+
+function toggleTimer () {
+    const button = document.getElementById('power');
+    const action = button.getAttribute('action');
+
+    clearInterval(intervalId);
+
+    if (action == 'start' || action == 'continue') {
+        intervalId = setInterval(() => {
+            timer += 1;
+            setTimer(timer);
+    }, 10);
+        button.setAttribute('action', 'pause');
+        button.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    } else if (action == 'pause') {
+        button.setAttribute('action', 'continue');
+        button.innerHTML = '<i class="fa-solid fa-play"></i>';
+    }
+};
+
+function markTime () {
+    marks.push(timer);
+    if(timer != 0){
+    addMarkToList(marks.length, timer);
+    }
+};
+
+function resetTimer () {
+    clearInterval(intervalId);
+    timer = 0;
+    setTimer(timer);
+    const button = document.getElementById('power');
+    button.setAttribute('action', 'start');
+    button.innerHTML = '<i class="fa-solid fa-play"></i>';
+};
+
+document.getElementById('power').addEventListener('click', toggleTimer);
+document.getElementById('mark').addEventListener('click', markTime);
+document.getElementById('reset').addEventListener('click', resetTimer);
+
+//  Changing watches and chronometer
+
+const digitalClockBtn = document.getElementById('botaodigital');
+const analogClockBtn = document.getElementById('botaoanalog');
+const digitalClock = document.querySelector('#digital-clock');
+const analogClock = document.querySelector('#analog-clock');
+const stopwatchBtn = document.querySelector('#botaostopwatch');
+const stopwatch = document.querySelector('#stopwatch');
+
+
+digitalClockBtn.addEventListener('click', () => {
+    digitalClock.style.display = 'flex';
+    analogClock.style.display = 'none';
+    stopwatch.style.display = 'none';
+});
+
+analogClockBtn.addEventListener('click', () => {
+    analogClock.style.display = 'flex';
+    digitalClock.style.display = 'none';
+    stopwatch.style.display = 'none';
+
+});
+
+stopwatchBtn.addEventListener('click', () => {
+    stopwatch.style.display = 'block'
+    analogClock.style.display = 'none';
+    digitalClock.style.display = 'none';
+});
